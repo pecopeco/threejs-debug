c97ee13dc75d2af82ea0b8d72f3fe3711a03c57f
pecopeco <yangpeco@qq.com>
2022-07-22 12:22:43 +0800

    update

--- src/injected_scripts.js
+++ src/injected_scripts.js
@@ -1,63 +1,55 @@
 // 传递threejs对象到content
+let timer
 function postContent () {
-  if (!three) {
+  if (!three || timer) {
     return
   }
-  // 将获取到的threejs对象传递给content_scripts
-  let postEvent = new CustomEvent('injected_event', { detail: { scene: transObj(three.scene), camera: transObj(three.camera) }})
-  window.dispatchEvent(postEvent)
+  // 限制每20毫秒只更新一次
+  timer = setTimeout(() => {
+    // 将获取到的threejs对象传递给content_scripts
+    let postEvent = new CustomEvent('injected_event', { detail: { scene: transObj(three.scene), camera: transObj(three.camera) }})
+    window.dispatchEvent(postEvent)
+    clearTimeout(timer)
+    timer = null
+  }, 20)
 }
 
-// 监听camera属性变化
+// 监听threeObj赋值
 let three
 Object.defineProperty(window, 'threeObj', { 
   set: function (newValue) {
     three = newValue
-    // Object.keys(three).map(key => {
-    //   listenerObj(three[key])
-    // })
-    let x = three.camera.position.x
-    let timer
-    Object.defineProperty(three.camera.position, 'x', {
-      get () {
-        return x
-      },
-      set (val) {
-        x = val
-        if (timer) return
-        timer = setTimeout(() => {
-          postContent()
-          clearTimeout(timer)
-          timer = null
-        }, 20)
-      }
+    Object.keys(three).map(key => {
+      listenerObj(three[key])
     })
   }
 })
 
-// function listenerObj (obj) {
-//   Object.keys(obj).map(key => {
-//     let temValue = obj[key]
-//     if (typeof temValue === 'object') {
-//       listenerObj(obj[key])
-//     } else {
-//       Object.defineProperty(obj, key, {
-//         get () {
-//           return temValue
-//         },
-//         set (val) {
-//           temValue = val
-//           if (timer) return
-//           timer = setTimeout(() => {
-//             postContent()
-//             clearTimeout(timer)
-//             timer = null
-//           }, 20)
-//         }
-//       })
-//     }
-//   })
-// }
+// 递归监听所有属性
+function listenerObj (obj) {
+  Object.keys(obj).map(key => {
+    if (
+      (!(isFinite(obj[key]) && typeof obj[key] === 'number') && !obj[key]) || // 为空
+      typeof obj[key] === 'function' || // 函数类型
+      key === 'children' // 多层嵌套
+    ) return
+
+    if (typeof obj[key] === 'object') {
+      listenerObj(obj[key])
+    } else {
+      let temValue = obj[key]
+      Object.defineProperty(obj, key, {
+        get () {
+          return temValue
+        },
+        set (val) {
+          temValue = val
+          postContent()
+        }
+      })
+    }
+  })
+}
 
 /**
  * @description: 转换特殊对象为普通对象
